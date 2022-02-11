0b6e768f738939337aa3659fd1b3398db516daa2
phuocpro1969 <phuocpro1969@gmail.com>
2022-02-11 15:40:23 +0700

    up source

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: process.env.HOST, 
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
+  dialect: "mysql",
+  pool: {
+    max: process.env.MAX_POOL,
+    min: process.env.MIN_POOL,
+    acquire: process.env.ACQUIRE_POOL,
+    idle: process.env.IDLE_POOL
+  }
+};
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+
+// init db
+const db = require("./models");
+db.sequelize.sync();
+
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
