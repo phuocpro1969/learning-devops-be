828012aef4b2badda42225c4ea30db1a1a6959c2
phuocpro1969 <phuocpro1969@gmail.com>
2022-02-11 22:54:52 +0700

    update env

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -6,9 +6,9 @@ module.exports = {
   DB: process.env.DB,
   dialect: "mysql",
   pool: {
-    max: process.env.MAX_POOL||5,
-    min: process.env.MIN_POOL||0,
-    acquire: process.env.ACQUIRE_POOL||30000,
-    idle: process.env.IDLE_POOL||10000
+    max: Integer.parseInt(process.env.MAX_POOL),
+    min: Integer.parseInt(process.env.MIN_POOL),
+    acquire: Integer.parseInt(process.env.ACQUIRE_POOL),
+    idle: Integer.parseInt(process.env.IDLE_POOL)
   }
 };
