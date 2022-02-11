953ff2d0f9ad9be0f0fd742c5b6cbf9147d05a2f
phuocpro1969 <phuocpro1969@gmail.com>
2022-02-11 20:41:38 +0700

    update docker-compose

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 require("./routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
